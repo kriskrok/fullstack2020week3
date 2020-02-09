1a2f27af5957987b74d58c00c91aed22736c4599
kriskrok <25787775+kriskrok@users.noreply.github.com>
2020-02-09 21:14:33 +0200

    Adds 3.9 puhelinluettelon backend step9

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
-const morgan = require('morgan')
 const app = express()
-const PORT = 3001
+const cors = require('cors')
+const morgan = require('morgan')
+
+const PORT = process.env.PORT || 3001
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token( 'body', (req, res) => JSON.stringify(req.body) )
 
