8f178b76a3115110efee094d70593353c9a424ea
kriskrok <25787775+kriskrok@users.noreply.github.com>
2020-02-09 13:20:41 +0200

    Adds 3.5: puhelinluettelon backend step5

--- index.js
+++ index.js
@@ -2,28 +2,32 @@ const express = require('express')
 const app = express()
 const PORT = 3001
 
+app.use(express.json())
+
 let persons = [
   {
-    "name": "Arto Hellas",
-    "number": "040-123456",
-    "id": 1
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: 1
   },
   {
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523",
-    "id": 2
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+    id: 2
   },
   {
-    "name": "Dan Abramov",
-    "number": "12-43-234345",
-    "id": 3
+    name: "Dan Abramov",
+    number: "12-43-234345",
+    id: 3
   },
   {
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122",
-    "id": 4
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+    id: 4
   }
 ]
+
+const generateID = () => Math.floor(Math.random() * (Number.MAX_SAFE_INTEGER - 1) + 1)
 
 app.get('/', (request, response) => {
   response.send('<h1>The cake is a lie!</h1>')
@@ -31,6 +35,19 @@ app.get('/', (request, response) => {
 
 app.get('/api/persons', (req, res) => {
   res.json(persons)
+})
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateID()
+  }
+
+  persons = persons.concat(person)
+
+  res.json(person)
 })
 
 app.get('/api/persons/:id', (req, res) => {
