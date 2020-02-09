a9a1f5b2bdf57a53cc6b528fc1987185437aca61
kriskrok <25787775+kriskrok@users.noreply.github.com>
2020-02-09 12:06:03 +0200

    Adds up to 3.4: puhelinluettelon backend step4

---
+++ README.md
@@ -0,0 +1,8 @@
+# Phonebook for week3 :space_invader:
+***
+
+- [rest of stuffs](https://github.com/kriskrok/fullstack2020/)
+
+<p align="center">
+  <img src="https://i.pinimg.com/originals/dd/e5/86/dde58600be2d94cf42a9681ee3eaa64e.jpg">
+</p>
---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require('express')
+const app = express()
+const PORT = 3001
+
+let persons = [
+  {
+    "name": "Arto Hellas",
+    "number": "040-123456",
+    "id": 1
+  },
+  {
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523",
+    "id": 2
+  },
+  {
+    "name": "Dan Abramov",
+    "number": "12-43-234345",
+    "id": 3
+  },
+  {
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122",
+    "id": 4
+  }
+]
+
+app.get('/', (request, response) => {
+  response.send('<h1>The cake is a lie!</h1>')
+})
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+
+  console.log(person)
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  
+  console.log(`id: ${id}`, persons.some(person => person.id === id))
+
+  persons = persons.filter(person => person.id !== id)
+
+  res.status(204).end()
+})
+
+app.get('/info', (req, res) => {
+  res.send(`<p>Phonebook has info for ${persons.length} people</br>${new Date()}</p>`)
+})
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
